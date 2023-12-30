65310f62e399d3b171b794149b619c8a1560fff8
Sampath Bandla <=>
2023-12-30 10:02:41 +0530

    changes done
    changes done

--- src/app/dashboard/user-management/user-management.component.ts
+++ src/app/dashboard/user-management/user-management.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-user-management',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-management.component.scss']
 })
 export class UserManagementComponent {
-
+	userCreateForm = new FormGroup({
+		full_name: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(25)]),
+		email: new FormControl('', [Validators.email]),
+		phone: new FormControl('', [Validators.required, Validators.minLength(10), Validators.maxLength(10)]),
+		role_id: new FormControl('', [Validators.required]),
+		username: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(25)]),
+		password: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(25)])
+	});
 }
