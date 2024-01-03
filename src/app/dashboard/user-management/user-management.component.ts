2a9ee7e04b8324e2ecfcb67b3bd283c87317656f
Sampath Bandla <sampath@pixelvide.com>
2024-01-03 20:58:21 +0530

    updated

--- src/app/dashboard/user-management/user-management.component.ts
+++ src/app/dashboard/user-management/user-management.component.ts
@@ -1,18 +1,47 @@
-import { Component } from '@angular/core';
-import {FormControl, FormGroup, Validators} from "@angular/forms";
+import { Component, OnInit, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
+import { RestApiService } from 'src/app/services/rest-api.service';
 
 @Component({
-  selector: 'app-user-management',
-  templateUrl: './user-management.component.html',
-  styleUrls: ['./user-management.component.scss']
+	selector: 'app-user-management',
+	templateUrl: './user-management.component.html',
+	styleUrls: ['./user-management.component.scss']
 })
-export class UserManagementComponent {
-	userCreateForm = new FormGroup({
-		full_name: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(25)]),
-		email: new FormControl('', [Validators.email]),
-		phone: new FormControl('', [Validators.required, Validators.minLength(10), Validators.maxLength(10)]),
-		role_id: new FormControl('', [Validators.required]),
-		username: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(25)]),
-		password: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(25)])
+export class UserManagementComponent implements OnInit {
+	api = inject(RestApiService);
+	fb = inject(FormBuilder);
+
+	userCreateForm = this.fb.group({
+		full_name: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(25)]],
+		email: ['', [Validators.email]],
+		phone: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(10)]],
+		role_id: ['', [Validators.required]],
+		username: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(25)]],
+		password: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(25)]],
 	});
+	ucf: any = this.userCreateForm.controls
+
+	roles = [];
+	formSubmitted = false;
+
+	ngOnInit(): void {
+		this.getRoles();
+	}
+
+	getRoles() {
+		this.api.getData("/user/roles").then(
+			(response: any) => {
+				this.roles = response?.data?.roles;
+			}
+		)
+	}
+
+	createUser() {
+		this.formSubmitted = true;
+		if (this.userCreateForm.invalid) return;
+	}
+
+	isFieldInvalid(name: any): boolean {
+		return (this.ucf[name].touched || this.ucf[name].dirty || this.formSubmitted) && this.ucf[name].invalid
+	}
 }
